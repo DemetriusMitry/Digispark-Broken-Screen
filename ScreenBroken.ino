4eccd29e34bb635134268d4e71a9f36ce2a22bf5
DemetriusMitry <54234499+DemetriusMitry@users.noreply.github.com>
2021-06-05 15:05:50 -0300

    Add files via upload

---
+++ ScreenBroken.ino
@@ -0,0 +1,28 @@
+#include "DigiKeyboardPtBr.h"
+void setup() {
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(1, HIGH); // LED on <--> Action start
+  
+  DigiKeyboardPtBr.sendKeyStroke(0);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.print("https://www.youtube.com/watch?v=uKisdcdVRl8&t=181s&ab_channel=ManuChaoManuChaoCanaloficialdoartista");
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(2000);
+  DigiKeyboardPtBr.sendKeyStroke(0x2b);
+  DigiKeyboardPtBr.sendKeyStroke(0x2b);
+  DigiKeyboardPtBr.sendKeyStroke(0x2b);
+  DigiKeyboardPtBr.sendKeyStroke(0x2b);
+  DigiKeyboardPtBr.sendKeyStroke(0x2b);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_F);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_F);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_F);
+  
+  digitalWrite(1, LOW);
+  for(;;){ /*empty*/ }
+}
